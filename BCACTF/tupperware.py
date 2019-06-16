4988d85654ca0ddb836c20a3b4feeeea134ab690
kuruwa2 <a93019050612@gmail.com>
2019-06-16 12:17:08 +0800

    Create tupperware.py

---
+++ BCACTF/tupperware.py
@@ -0,0 +1,53 @@
+a='four octogintacentillion, eight hundred and fifty-eight novemseptuagintacentillion, four hundred and eighty-seven octoseptuagintacentillion, seven hundred and three septenseptuagintacentillion, two hundred and seventeen quinseptuagintacentillion, six hundred and fifty-four quattorseptuagintacentillion, one hundred and sixty-eight treseptuagintacentillion, five hundred and seven duoseptuagintacentillion, three hundred and seventy-seven unseptuagintacentillion, one hundred and five septuagintacentillion, six hundred and thirty-four novemsexagintacentillion, two octosexagintacentillion, six hundred and forty-seven septensexagintacentillion, seven hundred and thirty-one sexsexagintacentillion, one hundred and twenty-eight quinsexagintacentillion, four hundred and ninety-nine quattuorsexagintacentillion, three hundred and seven tresexagintacentillion, two hundred and forty-four duosexagintacentillion, eight hundred and fifty-one unsexagintacentillion, four hundred and forty-one sexagintacentillion, ninety novemquinquagintacentillion, three hundred and fifty-seven octoquinquagintacentillion, eight hundred and sixty-five septenquinquagintacentillion, seven hundred and eleven sexquinquagintacentillion, thirty-three sexquinquagintacentillion, four hundred and forty-three quinquinquagintacentillion, four hundred and nineteen quattuorquinquagintacentillion, seven hundred and ninety-three trequinquagintacentillion, five hundred and thirty-one duoquinquagintacentillion, nine hundred and ninety-nine unquinquagintacentillion, eight hundred and eighty-three quinquagintacentillion, two hundred and sixty-one novemquadragintacentillion, five hundred and seventy-nine octoquadragintacentillion, eighty-six septenquadragintacentillion, seven hundred and three sexquadragintacentillion, two hundred and eighty-five quinquadragintacentillion, nine hundred and eighty-eight quattuorquadragintacentillion, five hundred and fifty trequadragintacentillion, three hundred and twenty-five duoquadragintacentillion, nine hundred and seventy unquadragintacentillion, five hundred and twelve quadragintacentillion, three hundred and forty-three novemtrigintacentillion, sixty-three octotrigintacentillion, three hundred and twelve septentrigintacentillion, nine hundred and sixty-five sextrigintacentillion, three hundred and twenty-seven quintrigintacentillion, five hundred and eighty quattuortrigintacentillion, nine hundred and seventy-eight tretrigintacentillion, two hundred and forty-one duotrigintacentillion, eight hundred and forty untrigintacentillion, four hundred and fifty-eight trigintacentillion, seven hundred and five novemviginticentillion, seven hundred and seventy-eight octoviginticentillion, five hundred and eighty-two septenviginticentillion, five hundred and four sexviginticentillion, six hundred and seven quinviginticentillion, six hundred and fifty-five quattuorviginticentillion, eight hundred and seventy-nine treviginticentillion, one hundred and fifty-one duoviginticentillion, eight hundred and twenty-eight unviginticentillion, one hundred and forty-nine viginticentillion, six hundred and ninety-four novemdecicentillion, five hundred and eighty-nine octodecicentillion, eight hundred and sixty-seven septendecicentillion, one hundred and ninety-eight sexdecicentillion, eight hundred and forty quindecicentillion, nine hundred and fifty-nine quattuordecicentillion, five hundred and ninety-eight tredecicentillion, four hundred and ninety-seven duodecicentillion, five hundred and ninety-seven undecicentillion, nine hundred and eighty-nine decicentillion, eight hundred and sixty-six novemcentillion, eighty-four octocentillion, six hundred and ninety-four septencentillion, five hundred and sixty sexcentillion, four hundred and ninety quinquacentillion, six hundred and seventy-five quattuorcentillion, eight hundred and one centretillion, seven hundred and eighty-six duocentillion, three hundred and forty-seven cenuntillion, seven hundred and thirty centillion, eight hundred and fifty-one novemnonagintillion, four hundred and thirty-eight octononagintillion, nine hundred and thirty-one septnonagintillion, two hundred and seventy-five sexnonagintillion, five hundred and eighty-four quinnonagintillion, nine hundred and eight duattuornonagintillion, one hundred and thirty-eight trenonagintillion, one hundred and ninety duononagintillion, four hundred and nine unnonagintillion, four hundred and eighty-one nonagintillion, eight hundred and eighty novemoctogintillion, nine hundred and eleven octooctogintillion, three hundred and nineteen septoctogintillion, four hundred and ninety-one sexoctogintillion, nine hundred and seventy-eight quinoctogintillion, six hundred and forty-four quattuoroctogintillion, eight hundred and eight treoctogintillion, thirty-six duooctogintillion, eight hundred and seventeen unoctogintillion, eight hundred and seventy-three octogintillion, five hundred and fourteen novemseptuagintillion, fifty-six octoseptuagintillion, five hundred and ninety septseptuagintillion, one hundred and thirty-five sexseptuagintillion, three hundred and thirty-one quinseptuagintillion, five hundred and seventy-eight quattuorseptuagintillion, one hundred and five treseptuagintillion, eight hundred and sixty-two duoseptuagintillion, eight hundred and twenty-one unseptuagintillion, four hundred and fifty-four septuagintillion, four hundred and fifty-four novemsexagintillion, six hundred and seventeen octosexagintillion, three hundred and seventy-five septsexagintillion, nine hundred and nineteen sexsexagintillion, eight hundred and seventy quinsexagintillion, two hundred and forty-five quattuorsexagintillion, three hundred and sixty-three tresexagintillion, four hundred and forty duosexagintillion, sixty-six unsexagintillion, three hundred and seventy-two sexagintillion, seven hundred novemquinquagintillion, fifty-three octoquinquagintillion, two hundred and sixty-three septenquinquagintillion, three hundred and sixty-two sexquinquagintillion, eight hundred and sixty-three quinquinquagintillion, six hundred and ninety-four quattuorquinquagintillion, one hundred and twenty-nine trequinquagintillion, one hundred and thirty-three duoquinquagintillion, seven hundred and sixty-five unquinquagintillion, four hundred and forty-one quinquagintillion, nine hundred and twenty-six novemquadragintillion, five hundred and thirty-three octoquadragintillion, six hundred and nine septenquadragintillion, five hundred and three sexquadragintillion, nine hundred and ninety-four quinquadragintillion, eighteen quattuorquadragintillion, five hundred and sixty-five trequadragintillion, three hundred and nineteen duoquadragintillion, three hundred and eighty-two unquadragintillion, nine hundred and thirteen quadragintillion, four hundred and fifty-four novemtrigintillion, one hundred and sixty-one octotrigintillion, four hundred and twenty-nine septentrigintillion, fifty-eight sextrigintillion, five hundred and five quintrigintillion, six hundred and fifty-five quattuortrigintillion, three hundred and fifty tretrigintillion, seven hundred and eighty duotrigintillion, nine hundred and eleven untrigintillion, two hundred and twenty-nine trigintillion, two hundred and three novemvigintillion, four hundred and thirty-two octovigintillion, three hundred and ninety-two septenvigintillion, nine hundred and sixty-seven sexvigintillion, two hundred and thirty-six quinvigintillion, one hundred and thirty-seven quattuorvigintillion, six hundred and forty-seven trevigintillion, seven hundred and eighty duovigintillion, three hundred and eight unvigintillion, nine hundred and sixty-six vigintillion, nine hundred and thirty-six novemdecillion, six hundred and ninety-one octodecillion, four hundred and thirty-nine septendecillion, six hundred and thirty-six sexdecillion, nine hundred and eighty quindecillion, fifty-eight quattuordecillion, two hundred and sixty-six tredecillion, six hundred and ninety-three duodecillion, four hundred and ninety-eight undecillion, five hundred and sixty-eight decillion, two hundred and thirty-five nonillion, four hundred and sixty-four octillion, three hundred and ten septillion, nine hundred and seven sextillion, six hundred and twenty-eight quintillion, seven hundred and eighty-five quadrillion, four hundred and thirty-three trillion, one hundred and five billion, ninety-one million, five hundred and twenty-six thousand, six hundred and thirty-nine'
+
+a=a.split(' ')
+
+c=['one','two','three','four','five','six','seven','eight','nine']
+d=['ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
+e=['eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+
+b=''
+f=''
+hflag = False
+tflag = False
+for i in range(len(a)):
+    if 'hundred' == a[i]:
+        hflag = True
+        for j in range(len(c)):
+            if c[j] in a[i-1]:
+                f += str(j+1)
+                break    
+    elif 'lion' in a[i]:
+        if hflag == False:
+            f += str(0)
+        hflag = False
+        if a[i-1] == 'hundred':
+            f += str(0)
+            f += str(0)
+        elif '-' in a[i-1]:
+            g = a[i-1].split('-')
+            for j in range(len(d)):
+                if d[j] == g[0]:
+                    f += str(j+1)
+                    break
+            for j in range(len(c)):
+                if c[j] == g[1]:
+                    f += str(j+1)
+        else:
+            for j in range(len(d)):
+                if d[j] == a[i-1]:
+                    f += str(j+1)
+                    f += str(0)
+                    tflag = True
+                    break
+                if e[j] == a[i-1]:
+                    f += str(j+11)
+                    break
+                if c[j] == a[i-1]:
+                    f += str(0)
+                    f += str(j+1)
+        assert(len(f)==3)
+        b += f
+        f = ''
+b += '526631'
+print(b)
